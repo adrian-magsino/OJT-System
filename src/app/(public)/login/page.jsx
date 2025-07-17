352f7e2ab0e7cf91860d7a94ba194d47e86c91b2
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-07-17 14:04:03 +0800

    removed login forms

--- src/app/(public)/login/page.jsx
+++ src/app/(public)/login/page.jsx
@@ -1,10 +1,23 @@
 import LoginForm from "@/app/components/auth/LoginForm";
 
 
+const handleLogin = () => {};
+
 export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">     
-      <LoginForm />
+      <div>
+        <h2 className="text-center text-2xl/9 font-bold tracking-tight">
+          Sign in to your account
+        </h2>
+        
+        <button
+          type="button"
+          className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2"
+        >
+          Sign in with CvSU Email
+        </button>
+      </div>
     </div>
   );
 }
