66f528f54cdca61a37aeb9b5dd3a9edb19ea01a3
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-08-12 15:01:51 +0800

    added config file for the website to contain metadata

--- src/components/layout/public/Footer.jsx
+++ src/components/layout/public/Footer.jsx
@@ -1,3 +1,4 @@
+import { siteConfig } from "@/lib/config/site";
 
 export default function Footer() {
   return (
@@ -5,7 +6,7 @@ export default function Footer() {
       <div className="mx-auto w-full max-w-screen-xl">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8 px-4 py-6">
           <div className="max-w-sm">
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">OJT SYSTEM</h2>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">{siteConfig.name}</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque.
             </p>
---
+++ src/lib/config/site.js
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "OJT Portal",
+  description: "Details about Training Establishments with history of working with CvSU",
+  logo: {
+    src: "/cvsu_logo.png",
+    alt: "CvSU Logo",
+  }
+}
