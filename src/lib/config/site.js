4a6360021fc8a65b2ebf73828738e009708d5819
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-08-14 13:54:59 +0800

    added external links in footer section

--- src/lib/config/site.js
+++ src/lib/config/site.js
@@ -6,3 +6,20 @@ export const siteConfig = {
     alt: "CvSU Logo",
   }
 }
+
+export const external_links = {
+  quick_links: {
+    main: "https://cvsu.edu.ph/",
+    about_us: "https://cvsu.edu.ph/mission-and-vision/",
+    connect_with_us: "https://cvsu.edu.ph/contact-us-2/"
+  },
+
+  socials: {
+    facebook: "https://www.facebook.com/CaviteStateU/"
+  },
+
+  contact: {
+    location: "Indang, Cavite, PHILIPPINES",
+  }
+}
+
